9874cd0382dae79316388152e8dda7fb9f49ebfd
Rob Nahf <rnahf@epscor.unm.edu>
2010-11-13 00:07:04 +0000

    added several test cases for identifier encoding tests.  Most don't pass yet.
    git-svn-id: file:///var/code/dataone/dataone/software/cicore/trunk/d1_integration@2628 a8e578cc-b067-0410-9529-a28e5a6d5a41

--- src/test/java/org/dataone/integration/D1ClientCNodeTest.java
+++ src/test/java/org/dataone/integration/D1ClientCNodeTest.java
@@ -47,149 +47,204 @@ import org.junit.rules.ErrorCollector;
  */
 public class D1ClientCNodeTest  {
 
-    private static final String cnUrl = "http://cn-dev.dataone.org/cn/";
-    private static final String mnUrl = "http://cn-dev.dataone.org/knb/";
-    private static final String badIdentifier = "ThisIdentifierShouldNotExist";
+	private static final String cnUrl = "http://cn-dev.dataone.org/cn/";
+	private static final String mnUrl = "http://cn-dev.dataone.org/knb/";
+	private static final String badIdentifier = "ThisIdentifierShouldNotExist";
 
-        
-    @Rule 
-    public ErrorCollector errorCollector = new ErrorCollector();
 
-//    @Before
-    public void setUp() throws Exception 
-    {
-        
-    }
-    
-    /**
-     * test the resolve() operation on Coordinating Nodes
-     */
+	@Rule 
+	public ErrorCollector errorCollector = new ErrorCollector();
+
+	//    @Before
+	public void setUp() throws Exception 
+	{
+
+	}
+
+	/**
+	 * test the resolve() operation on Coordinating Nodes
+	 */
+	@Test
+	public void testResolve() {
+		String idString = "test" + ExampleUtilities.generateIdentifier();
+		resolveRunner(idString);
+	}
+
+	@Test
+	public void testResolve_IdEncoding_Slash() {
+		String idString = "test:xxxx/yyy" + ExampleUtilities.generateIdentifier() ;
+		resolveRunner(idString);
+	}
+
     @Test
-    public void testResolve() {
-    	String idString = "test" + ExampleUtilities.generateIdentifier();
-    	resolveRunner(idString);
-    }
-    
-//    @Test
-    public void testResolve_IdEncoding_trailingSlash() {
-    	String idString = "test:" + ExampleUtilities.generateIdentifier() + "/";
-    	resolveRunner(idString);
-    }
-    
-//    @Test
-    public void testResolve_IdEncoding2() {
-    	String idString = "test:" + "Günther's DataSet#1" + ExampleUtilities.generateIdentifier();
-    	resolveRunner(idString);
-    }
+	public void testResolve_IdEncoding_trailingSlash() {
+		String idString = "test:" + ExampleUtilities.generateIdentifier() + "/";
+		resolveRunner(idString);
+	}
 
-    
-    
-    
-    /*
-     * procedure for creating and testing the return from resolve for different
-     * identifier strings
-     */
-    private void resolveRunner(String idString) {
-    	
-    	try {
-            D1Client d1 = new D1Client(cnUrl);
-            CNode cn = d1.getCN();
+	@Test
+	public void testResolve_idEncoding_rfc3986_pchar_unescaped_ascii() {
+	//	String idString = "test:" + ";:@&=$-_.+!*()',~" + ExampleUtilities.generateIdentifier();
+	//   mn/create doesn't allow ";" or "&", even though path-safe according to rfc3986
+		String idString = "test:" + ":@=$-_.+!*()',~" + ExampleUtilities.generateIdentifier();
+		resolveRunner(idString);
+	}
 
-            Identifier guid = new Identifier();
-            guid.setValue(idString);
-            
-            //insert a doc to resolve
-            printHeader("testResolve - node " + cnUrl);
-            d1 = new D1Client(mnUrl);
-            MNode mn = d1.getMN(mnUrl);
-            String principal = "uid%3Dkepler,o%3Dunaffiliated,dc%3Decoinformatics,dc%3Dorg";
-            AuthToken token = mn.login(principal, "kepler");
-            
-            //insert a data file
-            InputStream objectStream = this.getClass().getResourceAsStream("/org/dataone/client/tests/knb-lter-cdr.329066.1.data");
-            SystemMetadata sysmeta = ExampleUtilities.generateSystemMetadata(guid, ObjectFormat.TEXT_CSV);
-            
-            //insert EML file
-            //InputStream objectStream = this.getClass().getResourceAsStream("/org/dataone/client/tests/knb-lter-luq.76.2.xml");
-            //SystemMetadata sysmeta = ExampleUtilities.generateSystemMetadata(guid, ObjectFormat.EML_2_1_0));
-            
-            Identifier rGuid = null;
-            try {
-                rGuid = mn.create(token, guid, objectStream, sysmeta);
-                mn.setAccess(token, rGuid, "public", "read", "allow", "allowFirst");
-                checkEquals(guid.getValue(), rGuid.getValue());
-            } catch (Exception e) {
-                errorCollector.addError(new Throwable(createAssertMessage() + " error in testCreateScienceMetadata: " + e.getMessage()));
-            }
-        
-            ObjectLocationList oll = cn.resolve(token, rGuid);
+	
+	@Test
+	public void testResolve_idEncoding_rfc3986_pchar_escaped_ascii() {
+		String idString = "test:" + "`#%^{}[]\\|\"?<>" + ExampleUtilities.generateIdentifier();
+		resolveRunner(idString);
+	}
 
-            for (ObjectLocation ol : oll.getObjectLocationList()) {
-                System.out.println("Location: " + ol.getNodeIdentifier().getValue()
-                        + " (" + ol.getUrl() + ")");
-                checkTrue(ol.getUrl().contains(idString));
-            }
-        } catch (BaseException e) {
-            e.printStackTrace();
-            errorCollector.addError(new Throwable(createAssertMessage()
-                    + " error in testResolve: " + e.getMessage()));
-        }
-    }
+	@Test
+	public void testResolve_idEncoding_unicode_pct_escaped_bmp_lt255() {
+		String idString = "test-" + "£-" + ExampleUtilities.generateIdentifier();
+		resolveRunner(idString);
+	}
+	
+	
+	@Test
+	public void testResolve_idEncoding_unicode_pct_escaped_bmp_other_2byte() {
+		String idString = "test-" + "Ÿ-" + ExampleUtilities.generateIdentifier();
+		resolveRunner(idString);
+	}
 
-   
-    /**
-     * test the resolve() operation on Coordinating Nodes
-     */
-    @Test
-    public void testInvalidResolve() {
-        D1Client d1 = new D1Client(cnUrl);
-        CNode cn = d1.getCN();
+	@Test
+	public void testResolve_idEncoding_unicode_pct_escaped_bmp_3byte() {
+		String idString = "test-" + "\u20AC-\u0E09-" + ExampleUtilities.generateIdentifier();
+		resolveRunner(idString);
+	}
 
-        printHeader("testResolve - node " + cnUrl);
-        //AuthToken token = new AuthToken();
-        AuthToken token = null;
-        Identifier guid = new Identifier();
-        guid.setValue(badIdentifier);
-        try {
-            ObjectLocationList oll = cn.resolve(token, guid);
-            checkEquals("Should not reach this check, exception should have been generated.", "");
-        } catch (BaseException e) {
-            checkTrue(e instanceof NotFound);
-        }
-    }
-    
-    private static String createAssertMessage()
-    {
-        return "test failed at url " + cnUrl;
-    }
-    
-    
-    private void printHeader(String methodName)
-    {
-        System.out.println("\n***************** running test for " + methodName + " *****************");
-    }
-    
-    private void checkEquals(final String s1, final String s2)
-    {
-        errorCollector.checkSucceeds(new Callable<Object>() 
-        {
-            public Object call() throws Exception 
-            {
-                assertThat("assertion failed for host " + cnUrl, s1, is(s2));
-                return null;
-            }
-        });
-    }
-    
-    private void checkTrue(final boolean b)
-    {
-        errorCollector.checkSucceeds(new Callable<Object>() 
-        {
-            public Object call() throws Exception 
-            {
-                assertThat("assertion failed for host " + cnUrl, true, is(b));
-                return null;
-            }
-        });
-    }
+
+	@Test
+	public void testResolve_idEncoding_unicode_pct_escaped_supplementary_aegean() {
+		//to get supplementary Unicode code points into Strings, a couple extra steps are needed
+		// because these are beyond the 16 bit limit
+		int codepoint = 0x1010C;  // An Aegean number
+		char[] surrogatePair = Character.toChars(codepoint);
+		String supp = new String(surrogatePair);
+
+		String idString = "test-" + supp + "-" + ExampleUtilities.generateIdentifier();
+		resolveRunner(idString);
+	}
+
+
+
+
+	/**
+	 * test the resolve() operation on Coordinating Nodes
+	 */
+	@Test
+	public void testInvalidResolve() {
+		D1Client d1 = new D1Client(cnUrl);
+		CNode cn = d1.getCN();
+
+		printHeader("testResolve - node " + cnUrl);
+		//AuthToken token = new AuthToken();
+		AuthToken token = null;
+		Identifier guid = new Identifier();
+		guid.setValue(badIdentifier);
+		try {
+			ObjectLocationList oll = cn.resolve(token, guid);
+			checkEquals("Should not reach this check, exception should have been generated.", "");
+		} catch (BaseException e) {
+			checkTrue(e instanceof NotFound);
+		}
+	}
+
+
+
+	/*
+	 * a general procedure for creating and testing the return from resolve for different
+	 * identifier strings
+	 */
+	private void resolveRunner(String idString) {
+
+		try {
+			D1Client d1 = new D1Client(cnUrl);
+			CNode cn = d1.getCN();
+
+			Identifier guid = new Identifier();
+			guid.setValue(idString);
+
+			//insert a doc to resolve
+			printHeader("testResolve - node " + cnUrl);
+			d1 = new D1Client(mnUrl);
+			MNode mn = d1.getMN(mnUrl);
+			String principal = "uid%3Dkepler,o%3Dunaffiliated,dc%3Decoinformatics,dc%3Dorg";
+			AuthToken token = mn.login(principal, "kepler");
+
+			//insert a data file
+			InputStream objectStream = this.getClass().getResourceAsStream("/org/dataone/client/tests/knb-lter-cdr.329066.1.data");
+			SystemMetadata sysmeta = ExampleUtilities.generateSystemMetadata(guid, ObjectFormat.TEXT_CSV);
+
+			//insert EML file
+			//InputStream objectStream = this.getClass().getResourceAsStream("/org/dataone/client/tests/knb-lter-luq.76.2.xml");
+			//SystemMetadata sysmeta = ExampleUtilities.generateSystemMetadata(guid, ObjectFormat.EML_2_1_0));
+
+			Identifier rGuid = null;
+			try {
+				rGuid = mn.create(token, guid, objectStream, sysmeta);
+				mn.setAccess(token, rGuid, "public", "read", "allow", "allowFirst");
+				checkEquals(guid.getValue(), rGuid.getValue());
+			} catch (Exception e) {
+				errorCollector.addError(new Throwable(createAssertMessage() + " error in testCreateScienceMetadata: " + e.getMessage()));
+			}
+
+			// to prevent a null pointer exception
+			if (rGuid == null) {
+				rGuid = guid;
+			}
+
+			ObjectLocationList oll = cn.resolve(token, rGuid);
+
+			for (ObjectLocation ol : oll.getObjectLocationList()) {
+				System.out.println("Location: " + ol.getNodeIdentifier().getValue()
+						+ " (" + ol.getUrl() + ")");
+				checkTrue(ol.getUrl().contains(idString));
+			}
+		} catch (BaseException e) {
+			e.printStackTrace();
+			errorCollector.addError(new Throwable(createAssertMessage()
+					+ " error in testResolve: " + e.getMessage()));
+		}
+	}
+
+
+
+	private static String createAssertMessage()
+	{
+		return "test failed at url " + cnUrl;
+	}
+
+
+	private void printHeader(String methodName)
+	{
+		System.out.println("\n***************** running test for " + methodName + " *****************");
+	}
+
+	private void checkEquals(final String s1, final String s2)
+	{
+		errorCollector.checkSucceeds(new Callable<Object>() 
+				{
+			public Object call() throws Exception 
+			{
+				assertThat("assertion failed for host " + cnUrl, s1, is(s2));
+				return null;
+			}
+				});
+	}
+
+	private void checkTrue(final boolean b)
+	{
+		errorCollector.checkSucceeds(new Callable<Object>() 
+				{
+			public Object call() throws Exception 
+			{
+				assertThat("assertion failed for host " + cnUrl, true, is(b));
+				return null;
+			}
+				});
+	}
 }
