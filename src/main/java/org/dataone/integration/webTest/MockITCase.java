fdeec19bd3e932521667e91cbaa91a23ebac81d6
Rob Nahf <rnahf@epscor.unm.edu>
2011-07-07 04:53:15 +0000

    removed unneeded extending of ContextAwareTestCaseDataone in MockItCase
    git-svn-id: file:///var/code/dataone/dataone/software/cicore/trunk/d1_integration@4294 a8e578cc-b067-0410-9529-a28e5a6d5a41

--- src/main/java/org/dataone/integration/webTest/MockITCase.java
+++ src/main/java/org/dataone/integration/webTest/MockITCase.java
@@ -3,27 +3,18 @@ package org.dataone.integration.webTest;
 
 import static org.junit.Assert.assertTrue;
 
-import org.dataone.configuration.Settings;
-import org.dataone.integration.ContextAwareTestCaseDataone;
-import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 
 
-public class MockITCase extends ContextAwareTestCaseDataone {
+public class MockITCase { 
 	
-	@Override
-	protected String getTestDescription() {
-		return "a test class to exercise ability to report passes, fails, ignores, and exceptions";
-	}
-
 	
 	@Test
 	public void testTrue() 
 	{
 		assertTrue(true);
 	}
-
 
 
 	@Test
@@ -39,13 +30,11 @@ public class MockITCase extends ContextAwareTestCaseDataone {
 		;
 	}
 
+	
 	@Test
 	public void testException() {
 		// should throw a numerical exception of some type
 		float f = 12 / 0;
 		f++;
-	}
-	
-
-	
+	}	
 }
