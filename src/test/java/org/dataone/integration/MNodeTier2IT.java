60260c82636e338ebd9b8862220f60be46f33d34
Rob Nahf <rnahf@epscor.unm.edu>
2011-07-12 19:22:49 +0000

    draft version of Tier2 method tests
    git-svn-id: file:///var/code/dataone/dataone/software/cicore/trunk/d1_integration@4331 a8e578cc-b067-0410-9529-a28e5a6d5a41

---
+++ src/test/java/org/dataone/integration/MNodeTier2IT.java
@@ -0,0 +1,136 @@
+/**
+ * This work was created by participants in the DataONE project, and is
+ * jointly copyrighted by participating institutions in DataONE. For
+ * more information on DataONE, see our web site at http://dataone.org.
+ *
+ *   Copyright ${year}
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.dataone.integration;
+
+import java.io.InputStream;
+import java.util.Date;
+import java.util.Iterator;
+
+import org.dataone.client.D1Client;
+import org.dataone.client.MNode;
+import org.dataone.service.exceptions.BaseException;
+import org.dataone.service.exceptions.SynchronizationFailed;
+import org.dataone.service.types.Checksum;
+import org.dataone.service.types.ChecksumAlgorithm;
+import org.dataone.service.types.DescribeResponse;
+import org.dataone.service.types.Event;
+import org.dataone.service.types.Identifier;
+import org.dataone.service.types.Log;
+import org.dataone.service.types.LogEntry;
+import org.dataone.service.types.MonitorList;
+import org.dataone.service.types.Node;
+import org.dataone.service.types.ObjectFormatIdentifier;
+import org.dataone.service.types.ObjectList;
+import org.dataone.service.types.Permission;
+import org.dataone.service.types.Subject;
+import org.dataone.service.types.SystemMetadata;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
+
+/**
+ * Test the DataONE Java client methods.
+ * @author Rob Nahf
+ */
+public class MNodeTier2IT extends ContextAwareTestCaseDataone  {
+
+    private static final String TEST_MN_ID = "c3p0";
+    private static String format_text_csv = "text/csv";
+    private static String format_eml_200 = "eml://ecoinformatics.org/eml-2.0.0";
+    private static String format_eml_201 = "eml://ecoinformatics.org/eml-2.0.1";
+    private static String format_eml_210 = "eml://ecoinformatics.org/eml-2.1.0";
+    private static String format_eml_211 = "eml://ecoinformatics.org/eml-2.1.1";
+
+    private static final String idPrefix = "mnTier1:";
+    private static final String bogusId = "foobarbaz214";
+
+    private static String currentUrl;
+
+    
+
+	@Override
+	protected String getTestDescription() {
+		return "Test Case that runs through the Member Node Tier 2 API (Authorization) methods";
+	}
+
+	
+    @Test
+    public void testSetup() {
+    	System.out.println("text/csv: " + format_text_csv);//.getFormatName());
+    	System.out.println("text_eml_200: " + format_eml_200);//.getFormatName());
+    	System.out.println("text_eml_201: " + format_eml_201);//.getFormatName());
+    	System.out.println("text_eml_210: " + format_eml_210);//.getFormatName());
+    	System.out.println("done");
+    }
+	
+		
+	@Test
+	public void testIsAuthorized() {
+		Iterator<Node> it = getMemberNodeIterator();
+		while (it.hasNext()) {
+			currentUrl = it.next().getBaseURL();
+			MNode mn = D1Client.getMN(currentUrl);
+			printTestHeader("testIsAuthorized() vs. node: " + currentUrl);
+		
+			try {
+				// TODO: get a valid Identifier
+    			Identifier pid = new Identifier();
+    			pid.setValue(bogusId);
+				boolean success = mn.isAuthorized(null, pid, Permission.READ);
+				checkTrue(currentUrl,"isAuthorized response cannot be false. [Only true or exception].", success);
+			} 
+			catch (BaseException e) {
+				handleFail(currentUrl,e.getDescription());
+			}
+			catch(Exception e) {
+				e.printStackTrace();
+				handleFail(currentUrl,e.getClass().getName() + ": " + e.getMessage());
+			}	
+		}
+	}
+	
+	
+    @Test
+    public void testSetAccessPolicy()
+    {
+       Iterator<Node> it = getMemberNodeIterator();
+       while (it.hasNext()) {
+    	   currentUrl = it.next().getBaseURL();
+           MNode mn = D1Client.getMN(currentUrl);           
+           printTestHeader("testSetAccessPolicy() vs. node: " + currentUrl);
+
+           try {
+			// TODO: get a valid Identifier
+        	   Identifier pid = new Identifier();
+        	   pid.setValue(bogusId);
+        	   boolean success = mn.setAccessPolicy(null, pid, buildPublicReadAccessPolicy());
+			} 
+			catch (BaseException e) {
+				handleFail(currentUrl,e.getDescription());
+			}
+			catch(Exception e) {
+				e.printStackTrace();
+				handleFail(currentUrl,e.getClass().getName() + ": " + e.getMessage());
+			}	           
+       }
+    }
+
+}
