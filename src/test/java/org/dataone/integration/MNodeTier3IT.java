9150e936d5bb8e0d95e04878040cf394bd6db64e
Chris Jones <cjones@nceas.ucsb.edu>
2011-07-13 00:56:28 +0000

    Added MN Tier 3 integration tests for MNStorage.create(), update(), delete().  Discuss the use of create() for testing the other two methods with Rob since we need a pid to update or delete.
    git-svn-id: file:///var/code/dataone/dataone/software/cicore/trunk/d1_integration@4350 a8e578cc-b067-0410-9529-a28e5a6d5a41

---
+++ src/test/java/org/dataone/integration/MNodeTier3IT.java
@@ -0,0 +1,298 @@
+/**
+ * This work was created by participants in the DataONE project, and is
+ * jointly copyrighted by participating institutions in DataONE. For
+ * more information on DataONE, see our web site at http://dataone.org.
+ *
+ *   Copyright ${year}
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.dataone.integration;
+
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.util.Iterator;
+
+import org.dataone.client.D1Client;
+import org.dataone.client.MNode;
+import org.dataone.service.exceptions.IdentifierNotUnique;
+import org.dataone.service.exceptions.InsufficientResources;
+import org.dataone.service.exceptions.InvalidRequest;
+import org.dataone.service.exceptions.InvalidSystemMetadata;
+import org.dataone.service.exceptions.InvalidToken;
+import org.dataone.service.exceptions.NotAuthorized;
+import org.dataone.service.exceptions.NotFound;
+import org.dataone.service.exceptions.NotImplemented;
+import org.dataone.service.exceptions.ServiceFailure;
+import org.dataone.service.exceptions.UnsupportedType;
+import org.dataone.service.types.Identifier;
+import org.dataone.service.types.Node;
+import org.dataone.service.types.Session;
+import org.dataone.service.types.SystemMetadata;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class MNodeTier3IT extends ContextAwareTestCaseDataone {
+
+  private static String format_text_plain = "text/plain";
+  
+  private static String currentUrl;
+
+  @Before
+  public void setUp() throws Exception {
+  }
+
+  @After
+  public void tearDown() throws Exception {
+  }
+
+  /**
+   *  Test MNStorage.create() functionality
+   */
+  @Test
+  public void testCreate() {
+  	
+    Iterator<Node> it = getMemberNodeIterator();  	
+  	
+  	while (it.hasNext()) {
+  		
+      currentUrl = it.next().getBaseURL();
+      MNode mn = D1Client.getMN(currentUrl);
+      
+      // get a Session object
+  		Session session = ExampleUtilities.getTestSession();
+			String identifierStr = ExampleUtilities.generateIdentifier();
+			Identifier guid = new Identifier();
+			guid.setValue("mNodeTier3TestCreate." + identifierStr);
+			SystemMetadata sysMeta = new SystemMetadata();
+			InputStream textPlainSource;
+			
+			try {
+				// get some data bytes as an input stream
+				textPlainSource =
+					new ByteArrayInputStream("Plain text source".getBytes("UTF-8"));
+				
+				// build the system metadata object
+				sysMeta = 
+					ExampleUtilities.generateSystemMetadata(guid, format_text_plain, textPlainSource);
+				
+				// try the create
+				Identifier pid = mn.create(session, guid, textPlainSource, sysMeta);
+				
+				assertEquals(guid, pid);
+				
+			} catch (UnsupportedEncodingException e) {
+				e.printStackTrace();
+				
+			} catch (InvalidToken e) {
+		    fail("Unexpected error: " + e.getMessage());
+		    
+      } catch (ServiceFailure e) {
+  	    fail("Unexpected error: " + e.getMessage());
+  	    
+      } catch (NotAuthorized e) {
+  	    fail("Unexpected error: " + e.getMessage());
+  	    
+      } catch (IdentifierNotUnique e) {
+  	    fail("Unexpected error: " + e.getMessage());
+  	    
+      } catch (UnsupportedType e) {
+  	    fail("Unexpected error: " + e.getMessage());
+  	    
+      } catch (InsufficientResources e) {
+  	    fail("Unexpected error: " + e.getMessage());
+  	    
+      } catch (InvalidSystemMetadata e) {
+  	    fail("Unexpected error: " + e.getMessage());
+  	    
+      } catch (NotImplemented e) {
+  	    fail("Unexpected error: " + e.getMessage());
+  	    
+      } catch (InvalidRequest e) {
+  	    fail("Unexpected error: " + e.getMessage());
+  	    
+      }
+		}
+    
+    
+  }
+  
+  /**
+   *  Test MNStorage.update() functionality
+   */
+  @Test
+  public void testUpdate() {
+    
+  	Iterator<Node> it = getMemberNodeIterator();
+  	
+  	while ( it.hasNext() ) {
+  		
+  		currentUrl = it.next().getBaseURL();
+  		MNode mn = D1Client.getMN(currentUrl);
+  		
+  		Session session = ExampleUtilities.getTestSession();
+  		String identifierStr = ExampleUtilities.generateIdentifier();
+			Identifier guid = new Identifier();
+			guid.setValue("mNodeTier3TestUpdate." + identifierStr);
+			SystemMetadata sysMeta = new SystemMetadata();
+			InputStream textPlainSource;
+			Identifier newPid;
+			
+			try {
+	      // get some data bytes as an input stream
+	      textPlainSource = 
+	      	new ByteArrayInputStream("Plain text source".getBytes("UTF-8"));
+	      
+	      // build the system metadata object
+	      sysMeta = 
+	      	ExampleUtilities.generateSystemMetadata(guid, format_text_plain, textPlainSource);
+	      
+	      // get a pid to update
+	      Identifier pid = mn.create(session, guid, textPlainSource, sysMeta);
+	  		String newIdentifierStr = ExampleUtilities.generateIdentifier();
+	      newPid = new Identifier();
+	      newPid.setValue("mNodeTier3TestUpdate" + newIdentifierStr);
+	      
+	      Identifier updatedPid = 
+	      	mn.update(session, pid, textPlainSource, newPid, sysMeta);
+	      assertEquals(newPid, updatedPid);
+      
+			} catch (UnsupportedEncodingException e) {
+  	    fail("Unexpected error: " + e.getMessage());
+  	    
+      } catch (InvalidToken e) {
+  	    fail("Unexpected error: " + e.getMessage());
+  	    
+      } catch (ServiceFailure e) {
+  	    fail("Unexpected error: " + e.getMessage());
+  	    
+      } catch (NotAuthorized e) {
+  	    fail("Unexpected error: " + e.getMessage());
+  	    
+      } catch (IdentifierNotUnique e) {
+  	    fail("Unexpected error: " + e.getMessage());
+  	    
+      } catch (UnsupportedType e) {
+  	    fail("Unexpected error: " + e.getMessage());
+  	    
+      } catch (InsufficientResources e) {
+  	    fail("Unexpected error: " + e.getMessage());
+  	    
+      } catch (InvalidSystemMetadata e) {
+  	    fail("Unexpected error: " + e.getMessage());
+  	    
+      } catch (NotImplemented e) {
+  	    fail("Unexpected error: " + e.getMessage());
+  	    
+      } catch (InvalidRequest e) {
+  	    fail("Unexpected error: " + e.getMessage());
+  	    
+      } catch (NotFound e) {
+  	    fail("Unexpected error: " + e.getMessage());
+  	    
+      }
+
+  	}
+  	
+  }
+  
+  /**
+   *  Test MNStorage.delete() functionality
+   */
+  @Test
+  public void testDelete() {
+  	
+  	Iterator<Node> it = getMemberNodeIterator();
+  	
+  	while ( it.hasNext() ) {
+  		
+  	  currentUrl = it.next().getBaseURL();
+  		MNode mn = D1Client.getMN(currentUrl);
+  		
+  		Session session = ExampleUtilities.getTestSession();
+  		String identifierStr = ExampleUtilities.generateIdentifier();
+			Identifier guid = new Identifier();
+			guid.setValue("mNodeTier3TestDelete." + identifierStr);
+			SystemMetadata sysMeta = new SystemMetadata();
+			InputStream textPlainSource;
+			Identifier deletedPid;
+  		
+      try {
+	      // get some data bytes as an input stream
+	      textPlainSource = 
+	      	new ByteArrayInputStream("Plain text source".getBytes("UTF-8"));
+	      
+	      // build the system metadata object
+	      sysMeta = 
+	      	ExampleUtilities.generateSystemMetadata(guid, format_text_plain, textPlainSource);
+	      
+	      // get a pid to delete
+	      Identifier pid = mn.create(session, guid, textPlainSource, sysMeta);
+	      
+	      // try the delete
+	      deletedPid = mn.delete(session, pid);
+	      assertEquals(deletedPid, pid);
+	      
+      } catch (UnsupportedEncodingException e) {
+  	    fail("Unexpected error: " + e.getMessage());
+  	    
+      } catch (InvalidToken e) {
+  	    fail("Unexpected error: " + e.getMessage());
+  	    
+      } catch (ServiceFailure e) {
+  	    fail("Unexpected error: " + e.getMessage());
+  	    
+      } catch (NotAuthorized e) {
+  	    fail("Unexpected error: " + e.getMessage());
+  	    
+      } catch (IdentifierNotUnique e) {
+  	    fail("Unexpected error: " + e.getMessage());
+  	    
+      } catch (UnsupportedType e) {
+  	    fail("Unexpected error: " + e.getMessage());
+  	    
+      } catch (InsufficientResources e) {
+  	    fail("Unexpected error: " + e.getMessage());
+  	    
+      } catch (InvalidSystemMetadata e) {
+  	    fail("Unexpected error: " + e.getMessage());
+  	    
+      } catch (NotImplemented e) {
+  	    fail("Unexpected error: " + e.getMessage());
+  	    
+      } catch (InvalidRequest e) {
+  	    fail("Unexpected error: " + e.getMessage());
+  	    
+      } catch (NotFound e) {
+  	    fail("Unexpected error: " + e.getMessage());
+  	    
+      }
+
+  	}
+
+  }
+
+  @Override
+  protected String getTestDescription() {
+    return "Test Case that runs through the Member Node Tier 3 (Storage) API methods";
+    
+  }
+
+}
