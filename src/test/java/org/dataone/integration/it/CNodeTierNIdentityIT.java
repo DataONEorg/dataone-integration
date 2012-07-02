823d3c7ec7ff5ef0e461b8b090836a16a62c3a47
Rob Nahf <rnahf@epscor.unm.edu>
2012-07-02 22:22:54 +0000

    Reorganizing CNode tests by CN-component instead of api tier.
    git-svn-id: file:///var/code/dataone/dataone/software/cicore/trunk/d1_integration@9542 a8e578cc-b067-0410-9529-a28e5a6d5a41

--- src/test/java/org/dataone/integration/it/CNodeTierNIdentityIT.java
+++ src/test/java/org/dataone/integration/it/CNodeTierNIdentityIT.java
@@ -24,26 +24,247 @@ import java.util.Iterator;
 
 import org.dataone.client.CNode;
 import org.dataone.client.D1TypeBuilder;
+import org.dataone.client.auth.ClientIdentityManager;
 import org.dataone.service.exceptions.BaseException;
+import org.dataone.service.exceptions.IdentifierNotUnique;
+import org.dataone.service.exceptions.InvalidRequest;
 import org.dataone.service.exceptions.NotAuthorized;
 import org.dataone.service.exceptions.NotFound;
+import org.dataone.service.types.v1.Identifier;
 import org.dataone.service.types.v1.Node;
 import org.dataone.service.types.v1.Person;
 import org.dataone.service.types.v1.Subject;
 import org.dataone.service.types.v1.SubjectInfo;
 import org.junit.Ignore;
 import org.junit.Test;
+import org.springframework.util.StringUtils;
 
 /**
  * Test the DataONE Java client methods that focus on CN services.
  * @author Matthew Jones
  */
-public class CNodeTier2IdentityIT extends ContextAwareTestCaseDataone {
+public class CNodeTierNIdentityIT extends ContextAwareTestCaseDataone {
 
 	
 	private static String currentUrl;
 
-	 
+	/**
+     * test generating an Identifier containing the given fragment.
+     * Specifies the UUID syntax
+     */
+    @Test
+    public void testGenerateIdentifier() {
+    	setupClientSubject("testSubmitter");
+    	Iterator<Node> it = getCoordinatingNodeIterator();
+    	while (it.hasNext()) {
+    		currentUrl = it.next().getBaseURL();
+    		CNode cn = new CNode(currentUrl);
+    		printTestHeader("testGenerateIdentifier(...) vs. node: " + currentUrl);
+
+    		try {
+    			String fragment = "CNodeTier1Test";
+    			Identifier response = cn.generateIdentifier(null,"UUID",fragment);
+    			//TODO: UUID isn't supporting the fragment concept, so can't check 
+    			// this yet.
+//    			checkTrue(cn.getLatestRequestUrl(),"generateIdentifier(...) should return an Identifier object" +
+//    					" containing the given fragment: '" + fragment + "' Got: '" + response.getValue() + "'",
+//    					response.getValue().contains(fragment));
+    			checkTrue(cn.getLatestRequestUrl(),"generateIdentifier(...) should return a UUID-style" +
+    					"identifier with 5 hexidecimal segments separated by '-'s.  Got: " +
+    					response.getValue(),
+    					StringUtils.countOccurrencesOf(response.getValue(),"-") >=4);
+    					
+	
+    		} 
+    		catch (IndexOutOfBoundsException e) {
+    			handleFail(cn.getLatestRequestUrl(),"No Objects available to test against");
+    		}
+    		catch (BaseException e) {
+    			handleFail(cn.getLatestRequestUrl(),e.getClass().getSimpleName() + ": " + 
+    					e.getDetail_code() + ":: " + e.getDescription());
+    		}
+    		catch(Exception e) {
+    			e.printStackTrace();
+    			handleFail(currentUrl,e.getClass().getName() + ": " + e.getMessage());
+    		}
+    	}
+    }
+    
+    
+    /**
+     * tests validity of scheme names by trying an invalid scheme.
+     * It should throw an error.
+     */
+    @Test
+    public void testGenerateIdentifier_badScheme() {
+    	Iterator<Node> it = getCoordinatingNodeIterator();
+    	while (it.hasNext()) {
+    		currentUrl = it.next().getBaseURL();
+    		CNode cn = new CNode(currentUrl);
+    		printTestHeader("testGenerateIdentifier(...) vs. node: " + currentUrl);
+
+    		try {
+    			String fragment = "CNodeTier1Test";
+    			cn.generateIdentifier(null,"bloip",fragment);
+    			handleFail(cn.getLatestRequestUrl(),"generateIdentifier(...) with a bogus scheme should" +
+    					"throw an exception (should not have reached here)");
+    		} 
+    		catch (IndexOutOfBoundsException e) {
+    			handleFail(cn.getLatestRequestUrl(),"No Objects available to test against");
+    		}
+    		catch (InvalidRequest e) {
+    			// the expected outcome indicating good behavior :-)
+    		}
+    		catch (BaseException e) {
+    			handleFail(cn.getLatestRequestUrl(),e.getDescription());
+    		}
+    		catch(Exception e) {
+    			e.printStackTrace();
+    			handleFail(currentUrl,e.getClass().getName() + ": " + e.getMessage());
+    		}
+    	}
+    }
+
+	
+	
+	
+	/**
+     * Creates an identifier and reserves it.  Then checks that the client indeed
+     * has the reservation (hasReservation());
+     */
+    @Test
+    public void testHasReservation() {
+    	
+    	setupClientSubject("testSubmitter");
+    	Subject clientSubject = ClientIdentityManager.getCurrentIdentity();
+    	Iterator<Node> it = getCoordinatingNodeIterator();
+    	while (it.hasNext()) {
+    		currentUrl = it.next().getBaseURL();
+    		CNode cn = new CNode(currentUrl);
+    		printTestHeader("testHasReservation(...) vs. node: " + currentUrl);
+    		
+    		try {
+    			boolean response = false;
+//    			if (reservedIdentifier != null) {
+//    				response = cn.hasReservation(null,clientSubject,reservedIdentifier);
+//    			} else {
+    				Identifier pid = new Identifier();
+    				pid.setValue(ExampleUtilities.generateIdentifier());
+    				cn.reserveIdentifier(null, pid );
+    				response = cn.hasReservation(null,clientSubject,pid);
+//    			}
+    			checkTrue(cn.getLatestRequestUrl(),"response cannot be false. [Only true or exception].", response);
+    		} 
+    		catch (IndexOutOfBoundsException e) {
+    			handleFail(cn.getLatestRequestUrl(),"No Objects available to test against");
+    		}
+    		catch (BaseException e) {
+    			handleFail(cn.getLatestRequestUrl(),e.getDescription());
+    		}
+    		catch(Exception e) {
+    			e.printStackTrace();
+    			handleFail(currentUrl,e.getClass().getName() + ": " + e.getMessage());
+    		}
+    	}
+    }
+    
+    
+    /**
+     * Generates a new identifier, and tries hasReservation() without first reserving it.
+     * Expect a NotFound exception.  
+     */
+    @Test
+    public void testHasReservation_noReservation() {
+    	
+    	Subject clientSubject = ClientIdentityManager.getCurrentIdentity();
+    	
+    	Iterator<Node> it = getCoordinatingNodeIterator();
+    	while (it.hasNext()) {
+    		currentUrl = it.next().getBaseURL();
+    		CNode cn = new CNode(currentUrl);
+    		printTestHeader("testHasReservation(...) vs. node: " + currentUrl);
+
+    		try {
+    			boolean response = false;
+    			Identifier pid = new Identifier();
+    			pid.setValue(ExampleUtilities.generateIdentifier());
+    			response = cn.hasReservation(null,clientSubject, pid);
+
+    			checkTrue(cn.getLatestRequestUrl(),"response cannot be false. [Only true or exception].", 
+    					response);
+    		}
+    		catch (NotFound e) {
+    			; // this is desired behavior
+    		}
+//    		catch (NotAuthorized e) {
+//    			; // this is also acceptable
+//    		}
+    		catch (IndexOutOfBoundsException e) {
+    			handleFail(cn.getLatestRequestUrl(),"No Objects available to test against");
+    		}
+    		catch (BaseException e) {
+    			handleFail(cn.getLatestRequestUrl(),e.getClass().getSimpleName() + ": " + 
+    					e.getDetail_code() + ":: " + e.getDescription());
+    		}
+    		catch(Exception e) {
+    			e.printStackTrace();
+    			handleFail(currentUrl,e.getClass().getName() + ": " + e.getMessage());
+    		}
+    	}
+    }
+	
+    /**
+     * tests that reserveIdentifier works, then tries again to make sure
+     * that the identifier cannot be reserved again.
+     */
+    @Test
+    public void testReserveIdentifier() {
+    	setupClientSubject("testSubmitter");
+    	Iterator<Node> it = getCoordinatingNodeIterator();
+    	while (it.hasNext()) {
+    		currentUrl = it.next().getBaseURL();
+    		CNode cn = new CNode(currentUrl);
+    		printTestHeader("testReserveIdentifier(...) vs. node: " + currentUrl);
+
+    		boolean isReserved = false;
+    		try {
+    			Identifier pid = new Identifier();
+    			pid.setValue(ExampleUtilities.generateIdentifier());
+
+    			Identifier response = cn.reserveIdentifier(null,pid);
+    			checkTrue(cn.getLatestRequestUrl(),"reserveIdentifier(...) should return the given identifier",
+    					response.equals(pid));
+    			isReserved = true;
+    			// try again - should fail
+    			response = cn.reserveIdentifier(null,pid);
+
+    		} 
+    		catch (IndexOutOfBoundsException e) {
+    			handleFail(cn.getLatestRequestUrl(),"No Objects available to test against");
+    		}
+    		catch (IdentifierNotUnique e) {
+    			if (isReserved) {
+    				// then got the desired outcome
+    			} else {
+    				handleFail(cn.getLatestRequestUrl(),e.getDescription());
+    			}
+    		}
+    		catch (BaseException e) {
+    			handleFail(cn.getLatestRequestUrl(),e.getClass().getSimpleName() + ": " + 
+    					e.getDetail_code() + ":: " + e.getDescription());
+    		}
+    		catch(Exception e) {
+    			e.printStackTrace();
+    			handleFail(currentUrl,e.getClass().getName() + ": " + e.getMessage());
+    		}
+    	}
+    }
+
+ 	
+	
+	
+	
+	
 	/**
 	 * This is difficult to run over and over, because it require creating
 	 * new certificates for each new subject provided. 
