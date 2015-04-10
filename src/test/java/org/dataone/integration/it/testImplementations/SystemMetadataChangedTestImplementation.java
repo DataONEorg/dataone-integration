9b1f152eaacddc73d1577278bd42f8fc8d1836a7
andreib <andreib>
2015-04-10 19:09:19 +0000

    Committing some fixes to SystemMetadataChangedTestImplementations refs #6725
    git-svn-id: file:///var/code/dataone/dataone/software/cicore/trunk/d1_integration@15424 a8e578cc-b067-0410-9529-a28e5a6d5a41

--- src/test/java/org/dataone/integration/it/testImplementations/SystemMetadataChangedTestImplementation.java
+++ src/test/java/org/dataone/integration/it/testImplementations/SystemMetadataChangedTestImplementation.java
@@ -2,8 +2,6 @@ package org.dataone.integration.it.testImplementations;
 
 import static org.junit.Assert.assertTrue;
 
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
 import java.math.BigInteger;
 import java.util.Date;
 import java.util.Iterator;
@@ -12,21 +10,21 @@ import java.util.List;
 import org.apache.commons.collections.IteratorUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.dataone.client.v1.itk.D1Object;
-import org.dataone.client.v1.types.D1TypeBuilder;
 import org.dataone.configuration.Settings;
+import org.dataone.integration.APITestUtils;
 import org.dataone.integration.ContextAwareTestCaseDataone;
 import org.dataone.integration.ExampleUtilities;
 import org.dataone.integration.adapters.CNCallAdapter;
 import org.dataone.integration.adapters.CommonCallAdapter;
 import org.dataone.integration.webTest.WebTestDescription;
 import org.dataone.integration.webTest.WebTestName;
+import org.dataone.service.types.v1.AccessRule;
 import org.dataone.service.types.v1.Identifier;
 import org.dataone.service.types.v1.Node;
 import org.dataone.service.types.v1.NodeReference;
+import org.dataone.service.types.v1.Permission;
 import org.dataone.service.types.v1.Replica;
 import org.dataone.service.types.v2.SystemMetadata;
-import org.dataone.service.util.TypeMarshaller;
 import org.junit.Before;
 import org.junit.Ignore;
 
@@ -43,10 +41,10 @@ import org.junit.Ignore;
  * Assumptions made:
  * </p>
  * <ul>
+ *      <li>It's the originating MN's responsibility to update replica-holder MNs of sysmeta changes (as opposed to the CN's).</li>
  *      <li>We have one or more working CNs in the environment</li>
  *      <li>We have two or more working MNs in the environment</li>
- *      <li>The MNs/CNs are properly registered with each other.</li>
- *      <li>The metadata can by synced to the the other MNs in the environment</li>
+ *      <li>The MNs/CNs are properly registered.</li>
  *      <li>We know how long it takes the CN to sync MN data (if using newly-created data).</li>
  * </ul>
  * 
@@ -61,7 +59,6 @@ public class SystemMetadataChangedTestImplementation extends ContextAwareTestCas
     private static final long REPLICATION_TIME = 300000;    // FIXME "
     
     private static Log log = LogFactory.getLog(SystemMetadataChangedTestImplementation.class);
-    
     
     @Before
     public void setup() {
@@ -83,51 +80,34 @@ public class SystemMetadataChangedTestImplementation extends ContextAwareTestCas
             + "Some metadata is changed on an MN"
             + "The MN calls CN.updateSystemMetadata() to update its version of the system metadata. (Synchronous call.)"
             + "The MN also calls MN.updateSystemMetadata() on other replica-holding MNs, to update their system metadata. (Synchronous call.)</li>")
-    @Ignore("Not completely implemented yet")
-    public void testUpdateMetadata() {
+    public void testSystemMetadataChanged() {
      
         assertTrue("This test requires at least two MNs to work.", mnList.size() >= 2);
         assertTrue("This test requires at least one CN to work.", cnList.size() >= 1);
 
-        String subjectLabel = cnSubmitter;
-        
+        Identifier pid = null;
         Node mnNode = mnList.get(0);
-        CommonCallAdapter mn = new CommonCallAdapter(getSession(subjectLabel), mnNode, "v2");
+        CommonCallAdapter mn = new CommonCallAdapter(getSession("testRightsHolder"), mnNode, "v2");
         try {
-
-            // create some data to work with
-            Identifier pid = new Identifier();
-            //  pid.setValue("SystemMetadataChangedTest_" + ExampleUtilities.generateIdentifier());
-            pid.setValue("SystemMetadataChangedTest");    // FIXME <-- revert this later :p
-            byte[] contentBytes = ExampleUtilities.getExampleObjectOfType(DEFAULT_TEST_OBJECTFORMAT);
-            
-            D1Object d1o = new D1Object(pid, contentBytes,
-                    D1TypeBuilder.buildFormatIdentifier(DEFAULT_TEST_OBJECTFORMAT),
-                    D1TypeBuilder.buildSubject(subjectLabel),
-                    D1TypeBuilder.buildNodeReference("bogusAuthoritativeNode"));
-            
-            SystemMetadata sysmeta = TypeMarshaller.convertTypeFromType(d1o.getSystemMetadata(), SystemMetadata.class);
-            // null sysmeta.ReplicationPolicy should mean it gets 2 replicas by default
-            InputStream objectInputStream = new ByteArrayInputStream(contentBytes);
-
-            Identifier createdPid = mn.create(null, pid, objectInputStream, sysmeta);
-            
-            // CN should sync the data                         (alternatively, use existing data?)
+            AccessRule accessRule = APITestUtils.buildAccessRule("testRightsHolder", Permission.CHANGE_PERMISSION);
+            pid = new Identifier();
+            pid.setValue("testSystemMetadataChanged_" + ExampleUtilities.generateIdentifier());
+            Identifier createdPid = procureTestObject(mn, accessRule, pid);
+            // procureTestObject() is probably creating a new object
             Thread.sleep(SYNC_TIME);   // sleep long enough for CN sync to happen
             
             // modify the data
-            sysmeta = mn.getSystemMetadata(null, pid);
+            SystemMetadata sysmeta = mn.getSystemMetadata(null, pid);
             BigInteger newSerialVersion = sysmeta.getSerialVersion().add(BigInteger.ONE);
             sysmeta.setSerialVersion(newSerialVersion);
             Date nowIsh = new Date();
             sysmeta.setDateSysMetadataModified(nowIsh);
             boolean success = mn.updateSystemMetadata(null, pid, sysmeta);
             assertTrue("MN should have modified its own system metadata successfully.", success);
-            // TODO ^ does the MN update its own sysmeta the same way it would other MNs' ?
             
             // notify CN of the MN's change
             Node cnNode = cnList.get(0);
-            CNCallAdapter cn = new CNCallAdapter(getSession(subjectLabel), cnNode, "v2");
+            CNCallAdapter cn = new CNCallAdapter(getSession(cnSubmitter), cnNode, "v2");
             success = cn.updateSystemMetadata(null, createdPid, sysmeta);
             assertTrue("CN should have had its system metadata updated successfully.", success);
             
@@ -138,8 +118,8 @@ public class SystemMetadataChangedTestImplementation extends ContextAwareTestCas
             assertTrue("System metadata fetched from CN should now have updated serialVersion.", serialVersionMatches);
             assertTrue("System metadata fetched from CN should now have updated dateSysMetadataModified.", dateModifiedMatches );
             
-            // CN needs to run replication in order for metadata to contain replica info
-            // and in order for originating MN to update replica-holding MNs
+            // CN needs to run replication in order for sysmeta to contain replica info
+            // so we can test if we can update the sysmeta on the replica-holding MNs
             Thread.sleep(REPLICATION_TIME);
             
             fetchedCNSysmeta = cn.getSystemMetadata(null, createdPid);
@@ -158,7 +138,7 @@ public class SystemMetadataChangedTestImplementation extends ContextAwareTestCas
                     }
                 assertTrue("Should be able to find another MN that holds a replica.", replicaHolderNode != null);
                 
-                CommonCallAdapter replicaHolderMN = new CommonCallAdapter(getSession(subjectLabel), replicaHolderNode, "v2");
+                CommonCallAdapter replicaHolderMN = new CommonCallAdapter(getSession("testRightsHolder"), replicaHolderNode, "v2");
                 success = replicaHolderMN.updateSystemMetadata(null, createdPid, sysmeta);
                 assertTrue("Replica-holder MN (" + replica.getReplicaMemberNode().getValue() + ") should have had its system metadata updated successfully.", success);
                 
@@ -169,14 +149,16 @@ public class SystemMetadataChangedTestImplementation extends ContextAwareTestCas
                 assertTrue("System metadata fetched from replica-holder MN should now have updated dateSysMetadataModified", dateModifiedMatches );
             }
             
-
-            
-            
         } catch (Exception e) {
             assertTrue("Testing failed with exception: " + e.getMessage(), false);
             e.printStackTrace();
         } finally {
-            // TODO delete(pid) ... or ideally purge(pid)
+            // TODO purge(pid)
+            try {
+                if(pid != null)     
+                    mn.delete(null, pid);
+            } catch (Exception e2) {
+            }
         }
     }
     
