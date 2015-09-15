a5f8f33671ee8a431ff0eb3d1895bd39eb475e30
andreib <andreib>
2015-09-15 16:04:04 +0000

    Updating test class to match implementation change (different exception type)
    git-svn-id: file:///var/code/dataone/dataone/software/cicore/trunk/d1_integration@16312 a8e578cc-b067-0410-9529-a28e5a6d5a41

--- src/test/java/org/dataone/integration/it/apiTests/MNStorageV2IT.java
+++ src/test/java/org/dataone/integration/it/apiTests/MNStorageV2IT.java
@@ -163,7 +163,7 @@ public class MNStorageV2IT extends ContextAwareTestCaseDataone
     @Override
     @Test
     public void testUpdateSystemMetadata_InvalidSystemMetadata_NoSerialVersion() {
-        updSysmetaImpl.testUpdateSystemMetadata_InvalidSystemMetadata_NoSerialVersion(getMemberNodeIterator(), "v2");
+        updSysmetaImpl.testUpdateSystemMetadata_InvalidRequest_NoSerialVersion(getMemberNodeIterator(), "v2");
     }
     
     @Override
