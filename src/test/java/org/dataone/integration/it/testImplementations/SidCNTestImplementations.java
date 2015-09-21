5b33d76f9454d99baf00f52d273cace427a463d8
andreib <andreib>
2015-09-21 19:43:58 +0000

    Coordinating node sid tests had an object creation bug.
    git-svn-id: file:///var/code/dataone/dataone/software/cicore/trunk/d1_integration@16390 a8e578cc-b067-0410-9529-a28e5a6d5a41

--- src/test/java/org/dataone/integration/it/testImplementations/SidCNTestImplementations.java
+++ src/test/java/org/dataone/integration/it/testImplementations/SidCNTestImplementations.java
@@ -402,8 +402,8 @@ public class SidCNTestImplementations extends SidCommonTestImplementations {
                 Identifier p2 = createIdentifier("P2_", node);
                 Identifier s1 = createIdentifier("S1_", node);
                 
-                createTestObject(callAdapter, p1, s1, null, p2, true);
-                createTestObject(callAdapter, p2, s1, p1, null, true);
+                createTestObject(callAdapter, p1, s1, null, p2, false);
+                createTestObject(callAdapter, p2, s1, p1, null, false);
 
                 Identifier sid = s1;
                 Identifier pid = p2;
