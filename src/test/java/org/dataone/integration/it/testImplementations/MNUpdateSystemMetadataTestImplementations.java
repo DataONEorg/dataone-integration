55a4dade6786718df0292478e73490aa8dc2c991
andreib <andreib>
2015-11-04 16:43:22 +0000

    Bumped up the maximum replication time yet again since it took just over 40 minutes in practice (if replica for pid is resubmitted)
    git-svn-id: file:///var/code/dataone/dataone/software/cicore/trunk/d1_integration@16810 a8e578cc-b067-0410-9529-a28e5a6d5a41

--- src/test/java/org/dataone/integration/it/testImplementations/MNUpdateSystemMetadataTestImplementations.java
+++ src/test/java/org/dataone/integration/it/testImplementations/MNUpdateSystemMetadataTestImplementations.java
@@ -51,7 +51,7 @@ public class MNUpdateSystemMetadataTestImplementations extends UpdateSystemMetad
 
     private CNCallAdapter cn;
     private static final long METACAT_INDEXING_WAIT = 10000;
-    private static final long REPLICATION_WAIT = 30 * 60 * 1000; 
+    private static final long REPLICATION_WAIT = 45 * 60 * 1000; 
     
     public MNUpdateSystemMetadataTestImplementations(ContextAwareTestCaseDataone catc) {
         super(catc);
